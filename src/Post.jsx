ab8997f1c67681906d86e1839078d1797f98f3ff
ERB <67521326+emrebuzkiran@users.noreply.github.com>
2023-03-15 17:17:18 +0300

    update post.jsx
    ekstra olan linkler kaldırıldı

--- src/Post.jsx
+++ src/Post.jsx
@@ -13,29 +13,29 @@ const Post = () => {
         <div className="flex flex-col shadow-xl w-[80%] mx-auto space-y-4 p-4">
             <h2 className="text-xl">{post.title}</h2>
             <p className="">{post.selftext}</p>
-            <a href={post.url} target="_blank" className="text-blue-500">
-                Post'a eklenen link
-            </a>
-            <a
-                href={`https://www.reddit.com${post.permalink}`}
-                className="text-blue-500"
-                target="_blank"
-            >
-                Post Link
-            </a>
+            
             <div className="flex flex-row justify-center gap-4 text-center py-4">
                 <Link
                     to={`/`}
                     state={{ posts: location.state.posts }}
                     className="bg-blue-500 hover:bg-blue-700 w-fit text-white font-bold py-2 px-4 rounded"
                 >
-                    Back
+                    Geri
+                </Link>
+                <Link
+                    to={`https://www.reddit.com${post.permalink}`}
+                    state={{ posts: location.state.posts }}
+                    className="bg-blue-500 hover:bg-blue-700 w-fit text-white font-bold py-2 px-4 rounded"
+                >
+                    Post Link
                 </Link>
                 <button
                     onClick={() => onSourceClick(post.source)}
-                    className="bg-blue-500 hover:bg-blue-700 w-fit text-white font-bold py-2 px-4 rounded"
+                    
+                    className = "bg-blue-500 hover:bg-blue-700 w-fit text-white font-bold py-2 px-4 rounded"
+                    
                 >
-                    {post.source ? 'View Source' : 'No Source'}
+                    {post.source ? 'Kaynağı görüntüle' : 'Kaynak yok'}
                 </button>
             </div>
         </div>
