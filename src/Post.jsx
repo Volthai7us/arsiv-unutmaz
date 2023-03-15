5fa24afc0ceaf73fecc469ec43a41deeb1898144
ERB <67521326+emrebuzkiran@users.noreply.github.com>
2023-03-15 20:16:21 +0300

    kaynak yok uyarısı eklendi

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,11 +1,12 @@
 import { useParams, useLocation, Link } from 'react-router-dom'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Post = () => {
     const location = useLocation()
     const post = location.state.post
 
-    const onSourceClick = (url) => {
-        if (!url) return alert('Kaynak bulunamadı.')
+    const notify = (url) => {
+        if (!url) return toast.error("Kaynak Bulunamadı")
         window.open(url, '_blank')
     }
 
@@ -30,13 +31,25 @@ const Post = () => {
                     Post Link
                 </Link>
                 <button
-                    onClick={() => onSourceClick(post.source)}
+                    onClick={() => notify(post.source)}
                     
                     className = "bg-blue-500 hover:bg-blue-700 w-fit text-white font-bold py-2 px-4 rounded"
                     
                 >
                     {post.source ? 'Kaynağı görüntüle' : 'Kaynak yok'}
                 </button>
+                {<ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="colored"
+                />}
             </div>
         </div>
     )
